f04eeb964dfb29c26b9e7132085baf349d88e18e
anishreddy13 <anishreddy1373@gmail.com>
2026-06-20 20:50:48 +0530

    fix: remove duplicate footer and nav from stats page

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -51,10 +51,6 @@ export default function StatsPage() {
 
   return (
     <PlatformShell>
-      <AnimatedBackground />
-      <CursorEffect />
-      <Navbar />
-
       <main className="relative z-10 pt-24 sm:pt-32 pb-20">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           
@@ -92,8 +88,6 @@ export default function StatsPage() {
 
         </div>
       </main>
-
-      <Footer />
     </PlatformShell>
   );
 }
